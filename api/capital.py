e750ee52fb76179a2cd2ca1aac17c83fad511dee
Mathieu Roberts <mathieulroberts@gmail.com>
2023-02-15 15:18:26 -0800

    capital test

---
+++ api/capital.py
@@ -0,0 +1,28 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+
+        if "country" in dic:
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + dic["country"])
+            data = r.json()
+            capital = data[0]["capital"][0]
+            message = f"The capital of {dic['country']} is {str(capital)}."
+
+        else:
+            message = "Give Country name please"
+
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+
+        self.wfile.write(message.encode())
+
+        return
