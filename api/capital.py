62146a2500f18e651f7510e3dafb2995fb88f0da
Mathieu Roberts <mathieulroberts@gmail.com>
2023-02-15 15:57:47 -0800

    capital country test

--- api/capital.py
+++ api/capital.py
@@ -20,7 +20,7 @@ class handler(BaseHTTPRequestHandler):
             url = "https://restcountries.com/v3.1/capital/"
             r = requests.get(url + dic["capital"])
             data = r.json()
-            capital = data[0]["capital"][0]
+            capital = data[0]['name']["common"]
             message = f"{str(capital)} is The capital of {dic['country']}."
 
         else:
